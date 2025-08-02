da7f15b1c4f66917a96ce442882ee32be331b5d0
Vincent-mutwiri <Vincentmutwiri9@gmail.com>
2025-08-02 18:43:09 +0300

    fixed the TypeScript error by updating the key prop in the ProductCard  component within the products page.

--- app/(main)/products/page.tsx
+++ app/(main)/products/page.tsx
@@ -23,7 +23,7 @@ export default function ProductsPage() {
       ) : (
         <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
           {products?.map((product) => (
-            <ProductCard key={product._id} product={product} />
+            <ProductCard key={product._id?.toString()} product={product} />
           ))}
         </div>
       )}
